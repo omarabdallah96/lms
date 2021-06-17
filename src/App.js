14fab305e8a5d367cd300c3c968ce8a970230ce1
omar <omarlb512@gmail.com>
2021-06-17 12:48:44 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Reports from './pages/Reports';
+import Products from './pages/Products';
+import Student from './pages/Student';
+
+function App() {
+  return (
+    <>
+        <Home />
+      
+        
+        
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Student.js
@@ -0,0 +1,127 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    background:'#3f51b5',
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 700,
+  },
+});
+
+const Student = () => {
+  const classes = useStyles();
+  const [product, setProduct] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const getProductData = async () => {
+    try {
+      const data = await axios.get(
+        "http://localhost:8000/api/student"
+      );
+      console.log(data.data);
+      setProduct(data.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getProductData();
+  }, []);
+  return (
+    <div className="App">
+      <h1>Student table</h1>
+      <input
+        type="text"
+        placeholder="Search here"
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      />
+
+      {/* {product
+        .filter((item) => {
+          if (search == "") {
+            return item;
+          } else if (item.name.toLowerCase().includes(search.toLowerCase())) {
+            return item;
+          }
+        })
+        .map((item) => {
+          return (
+            <p>
+              {item.name} - {item.price}
+            </p>
+          );
+        })} */}
+
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>First name</StyledTableCell>
+              <StyledTableCell >Last name</StyledTableCell>
+              <StyledTableCell >blood type</StyledTableCell>
+
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {product
+              .filter((item) => {
+                if (search == "") {
+                  return item;
+                } else if (
+                  item.fname.toLowerCase().includes(search.toLowerCase())
+                ) {
+                  return item;
+                }
+              })
+              .map((item) => {
+                return (
+                  <StyledTableRow key={item.id}>
+                    <StyledTableCell component="th" scope="row">
+                      {item.fname}
+                    </StyledTableCell>
+                    <StyledTableCell >
+                      {item.lname}
+                    </StyledTableCell>
+                    <StyledTableCell >
+                      {item.bloodtype}
+                      
+                    </StyledTableCell>
+                  </StyledTableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+};
+
+export default Student;
